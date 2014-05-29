882753f6860f33e99c4a9237080f62e07ddece82
Hashrocket Workstation <dev@hashrocket.com>
2014-05-29 18:07:33 -0400

    Initial views

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,16 @@
+class PagesController < ApplicationController
+
+	def home
+		render :home
+	end
+
+    def about
+    	render :about
+    end
+
+    def contact
+        render :contact
+    end 
+     
+end
+
---
+++ db/migrate/20140522231954_create_product.rb
@@ -0,0 +1,11 @@
+class CreateProduct < ActiveRecord::Migration
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.string :description 
+      t.string :price
+      t.string :image
+      t.timestamps    	
+    end
+  end
+end
