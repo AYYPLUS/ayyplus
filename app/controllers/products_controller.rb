dc252ee84ff860bdf942349fa2e61ca9eee228d2
Hashrocket Workstation <dev@hashrocket.com>
2014-06-01 15:48:36 -0400

    user edits product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,22 @@ class ProductsController < ApplicationController
 	def index
 		@products = Product.all
 	end
+	def new
+		@product =Product.new
+	end
+	def create
+		Product.create(product_params)
+		redirect_to(products_path )
+	end
+	def edit
+		@product = Product.find(params[:id])
+	end
+	def update
+		@product = Product.find(params[:id])
+		@product.update_attributes(product_params)
+		redirect_to(products_path)
+	end
+	def product_params
+		params.require(:product).permit(:name, :price, :description)
+	end
 end
